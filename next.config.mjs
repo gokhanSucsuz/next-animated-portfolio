be8134af5f73c6f58ac70d63d3bf17560f07cb6d
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-10-17 01:21:37 +0300

    ....

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   images: {
     remotePatterns: [
       {
-        loader: "custom",
-        loaderFile: "./loader.js",
         protocol: "https",
         hostname: "images.pexels.com",
       },
